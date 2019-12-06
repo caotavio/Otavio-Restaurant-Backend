85cc3fff946cf97601b0b843734c316884382df3
caotavio <caotavio@hotmail.com>
2019-12-06 21:25:02 +0000

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
+const allowCors = require('./cors')
 
 const app = express();
 const distPath = __dirname + '/dist/';
 
 app.use(express.static(distPath));
+app.use(allowCors);
 
 // app.get('/*', function(req,res) {   
 //     res.sendFile(path.join(`${distPath}index.html`));
