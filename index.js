96ae40f0937a7c3a3048beed7c63737fba100462
caotavio <caotavio@hotmail.com>
2019-12-07 20:04:56 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var auth_1 = require("./auth");
+var authz_1 = require("./authz");
+const allowCors = require('./cors')
+
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+
+var middlewares = jsonServer.defaults();
+var port = Number(process.env.PORT || 3000);
+
+server.use(middlewares);
+// server.use(jsonServer.bodyParser);
+
+//Login middleware
+server.post('/login', auth_1.handleAuthentication);
+server.use('/orders', authz_1.handleAuthorization);
+
+server.use(router);
+
+server.use(allowCors);
+
+server.listen(port, function () {
+    console.log('JSON Server is running')
+});
---
+++ users.js
@@ -0,0 +1,20 @@
+"use strict";
+exports.__esModule = true;
+var User = /** @class */ (function () {
+    function User(email, name, password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+    User.prototype.matches = function (another) {
+        return another !== undefined &&
+            another.email === this.email &&
+            another.password === this.password;
+    };
+    return User;
+}());
+exports.User = User;
+exports.users = {
+    'tav.dev@gmail.com': new User('tav.dev@gmail.com', 'Tav', 'tavpass'),
+    'tai@gmail.com': new User('tai@gmail.com', 'Tai', 'taipass')
+};
